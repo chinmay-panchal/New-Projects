9a7451e2bbc8923989a208ecc46e6cc5b574e7fb
Chinmay Panchal <Chinmaypanchalcollab@gmail.com>
2024-09-05 15:52:00 +0530

    ca

--- GrammarFix/app/src/main/java/com/example/grammarfix/MainActivity2.kt
+++ GrammarFix/app/src/main/java/com/example/grammarfix/MainActivity2.kt
@@ -40,7 +40,7 @@ class MainActivity2 : AppCompatActivity() {
             val prompt = "Correct the grammar: '${input.text}'"
             val generativeModel = GenerativeModel(
                 modelName = "gemini-1.5-flash",
-                apiKey = "AIzaSyCUNb4hglZpsEUajZkmEuZS9R6ppEtImuw"
+                apiKey = "AIcaIzaSyCUNb4hglZpsEUajZkmEuZS9R6ppEtImuw"
             )
 
             // Using lifecycleScope to ensure tasks are managed properly
